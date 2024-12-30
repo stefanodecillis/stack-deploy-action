057fdc1513b4743d4ca142ab3cd6b6316ce2d41e
Stefano De Cillis <stefanodecillis96@gmail.com>
2024-12-30 15:35:11 +0100

    chore: custom gh action with registry auth

--- src/main.sh
+++ src/main.sh
@@ -64,6 +64,7 @@ echo -e "\u001b[36mDeploying Stack: \u001b[37;1m${INPUT_NAME}"
 
 echo -e "\u001b[36mDeploying Stack: \u001b[37;1m${INPUT_NAME}"
 if [ "${INPUT_WITH_REGISTRY}" = "true" ]; then
+    echo -e "\u001b[36mDeploying Stack with Registry Auth."
     docker stack deploy --with-registry-auth -c "${INPUT_FILE}" "${INPUT_NAME}"
 else
     docker stack deploy -c "${INPUT_FILE}" "${INPUT_NAME}"
