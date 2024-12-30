f71dfc099572ba4ce657ecc52f657585a838c02e
Stefano De Cillis <stefanodecillis96@gmail.com>
2024-12-30 19:22:57 +0100

    add gcloud

--- src/main.sh
+++ src/main.sh
@@ -63,12 +63,12 @@ fi
 echo -e "\u001b[36mDeploying Stack: \u001b[37;1m${INPUT_NAME}"
 
 if [ ! -d "$HOME/google-cloud-sdk" ]; then
-    apt-get update
-    apt-get install -y lsb-release
+    apt update
+    apt install -y lsb-release
     export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)";
     echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list;
     curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - ;
-    apt-get update && apt-get install google-cloud-sdk;
+    apt update && apt install google-cloud-sdk;
 fi
 
 echo -e "\u001b[36mDeploying Stack: \u001b[37;1m${INPUT_NAME}"
